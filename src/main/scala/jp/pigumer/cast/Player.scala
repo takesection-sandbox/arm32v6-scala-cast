2a8fb5b3eaffe93fa23b3318bd369f7138153919
Shigeki Shoji <takesection@gmail.com>
2018-05-27 01:55:13 +0900

    WIP

--- src/main/scala/jp/pigumer/cast/Player.scala
+++ src/main/scala/jp/pigumer/cast/Player.scala
@@ -1,36 +1,48 @@
 package jp.pigumer.cast
 
-import java.io.ByteArrayInputStream
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, OutputStream}
 
 import akka.stream.scaladsl.Flow
 import akka.util.ByteString
 import javax.sound.sampled.AudioFormat.Encoding
-import javax.sound.sampled.{AudioFormat, AudioInputStream, AudioSystem}
+import javax.sound.sampled._
 
 object Player {
 
-  val mixerInfo = AudioSystem.getMixerInfo()
+  val mixerInfo: Array[Mixer.Info] = AudioSystem.getMixerInfo()
 
   val convert = (bytes: ByteString) ⇒ {
-      val originalAudioInput = AudioSystem.getAudioInputStream(new ByteArrayInputStream(bytes.toArray))
-      val originalFormat = originalAudioInput.getFormat()
-      val converted: AudioInputStream = AudioSystem.getAudioInputStream(
-        new AudioFormat(
-          Encoding.PCM_SIGNED,
-          originalFormat.getSampleRate,
-          16,
-          originalFormat.getChannels,
-          originalFormat.getChannels * 2,
-          originalFormat.getSampleRate,
-          false
-        ),
-        originalAudioInput)
-      converted
+
+    def write(f: OutputStream ⇒ Unit) = {
+      val output = new ByteArrayOutputStream()
+      try {
+        f(output)
+        ByteString(output.toByteArray)
+      } finally {
+        output.close()
+      }
     }
 
+    val originalAudioInput = AudioSystem.getAudioInputStream(new ByteArrayInputStream(bytes.toArray))
+    val originalFormat = originalAudioInput.getFormat()
+    val converted: AudioInputStream = AudioSystem.getAudioInputStream(
+      new AudioFormat(
+        Encoding.PCM_SIGNED,
+        originalFormat.getSampleRate,
+        16,
+        originalFormat.getChannels,
+        originalFormat.getChannels * 2,
+        originalFormat.getSampleRate,
+        false
+      ),
+      originalAudioInput)
+
+    write(output ⇒ AudioSystem.write(converted, AudioFileFormat.Type.AU, output))
+  }
+
   val play = (index: Int) ⇒
-    Flow[ByteString].map { bytes ⇒
-      val input = convert(bytes)
+    Flow[ByteString].map(convert).map { bytes ⇒
+      val input = AudioSystem.getAudioInputStream(new ByteArrayInputStream(bytes.toArray))
       val mixer = mixerInfo(index)
       val clip = AudioSystem.getClip(mixer)
       clip.open(input)
